10c22f5421f37b4964fe96250ba31a0fbe530d8c
Prathiksha D V <prathikshadv2005@gmail.com>
2026-03-20 22:29:23 +0530

    added product_of_array_except_self solution

---
+++ arrays/product_of_array_except_self.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+         int n = nums.size();
+    vector<int> answer(n, 1);
+
+    // Step 1: Prefix product
+    for (int i = 1; i < n; i++) {
+        answer[i] = answer[i - 1] * nums[i - 1];
+    }
+
+    // Step 2: Suffix product
+    int suffix = 1;
+    for (int i = n - 1; i >= 0; i--) {
+        answer[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return answer;
+    }
+};
