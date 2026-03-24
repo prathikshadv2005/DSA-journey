9945890e262a17078b9a343fa0df567a23c970ca
Prathiksha D V <prathikshadv2005@gmail.com>
2026-03-24 23:36:20 +0530

    Create find_duplicate_number.cpp

---
+++ arrays/find_duplicate_number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+    int fast = nums[0];
+
+    // Step 1: Detect cycle
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow != fast);
+
+    // Step 2: Find entrance
+    slow = nums[0];
+    while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+
+    return slow;
+    }
+};
