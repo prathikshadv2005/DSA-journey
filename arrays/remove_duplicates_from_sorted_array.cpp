77ef0b995026d7fe10a8ddbb49a88411535f6f6f
Prathiksha D V <prathikshadv2005@gmail.com>
2026-03-27 22:30:11 +0530

    Create remove_duplicates_from_sorted_array.cpp

---
+++ arrays/remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+    if (nums.empty()) return 0;
+
+    int i = 0;
+
+    for (int j = 1; j < nums.size(); j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+
+    return i + 1;
+}
