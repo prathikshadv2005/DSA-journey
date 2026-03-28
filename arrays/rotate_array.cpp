e3c9077e06cdb699d4ae6ba59a0152ccf38f9ce8
Prathiksha D V <prathikshadv2005@gmail.com>
2026-03-28 23:10:09 +0530

    Create rotate_array.cpp

---
+++ arrays/rotate_array.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void rotate(vector<int>& nums, int k) {
+    int n = nums.size();
+    k = k % n;  // handle k > n
+
+    reverse(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+}
